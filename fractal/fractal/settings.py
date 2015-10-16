08ef849d95ec88ed63e2129e9851df4a4b023015
Chris Jack <cjack@csag.uct.ac.za>
2015-10-16 12:36:25 +0200

    updated google oauth2 key and secret

--- fractal/fractal/settings.py
+++ fractal/fractal/settings.py
@@ -131,3 +131,28 @@ AUTHENTICATION_BACKENDS = (
     'social.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
+
+# General Social Auth stuff
+SOCIAL_AUTH_COMPLETE_URL_NAME  = 'socialauth_complete'
+
+# Linkedin Social Auth stuff
+SOCIAL_AUTH_LINKEDIN_OAUTH2_KEY = '789k7081s8i0lw'
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SECRET = 'zkledww6gPwMxBXl'
+# Add email to requested authorizations.
+SOCIAL_AUTH_LINKEDIN_OAUTH2_SCOPE = ['r_basicprofile', 'r_emailaddress']
+# Add the fields so they will be requested from linkedin.
+SOCIAL_AUTH_LINKEDIN_OAUTH2_FIELD_SELECTORS = ['email-address', 'headline', 'industry']
+# Arrange to add the fields to UserSocialAuth.extra_data
+SOCIAL_AUTH_LINKEDIN_OAUTH2_EXTRA_DATA = [('id', 'id'),
+                                   ('firstName', 'first_name'),
+                                   ('lastName', 'last_name'),
+                                   ('emailAddress', 'email_address'),
+                                   ('headline', 'headline'),
+                                   ('industry', 'industry')]
+
+# Google Oauth2 settings
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '562563677306-rt72129c8f1s9mtist9uuo7c9etgkb9h.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'm6zy6r28KGE9wSf6cwLeNR26'
+SOCIAL_AUTH_GOOGLE_STATE_PARAMETER = False
+
+#SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
